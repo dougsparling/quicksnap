f4f85429ebb2e105e117523cc575ee43c36d6043
Doug Sparling <doug@cyberdeck.dev>
2025-04-27 13:40:26 +0900

    Add compose.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -15,6 +16,14 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
 
     buildTypes {
@@ -40,12 +49,24 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    // Camera
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.viewfinder)
     implementation(libs.androidx.lifecycle.service)
     implementation(libs.androidx.camera.video)
+
+    // Compose + Compose UI
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.foundation)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/kotlin/dev/cyberdeck/qs/MainActivity.kt
+++ app/src/main/kotlin/dev/cyberdeck/qs/MainActivity.kt
@@ -2,14 +2,39 @@ package dev.cyberdeck.qs
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val serviceIntent = Intent(this, CameraService::class.java)
         startForegroundService(serviceIntent)
-        finish()
+
+        setContent {
+            val colorScheme = when {
+                isSystemInDarkTheme() -> dynamicDarkColorScheme(LocalContext.current)
+                else -> dynamicLightColorScheme(LocalContext.current)
+            }
+
+            MaterialTheme(colorScheme = colorScheme) {
+                Scaffold { insets ->
+                    Box(modifier = Modifier.padding(insets)) {
+                        Text("hello world")
+                    }
+                }
+            }
+        }
     }
 }
